b2ed38450a0f55f36642ec043ed7db108bd6a054
natansilvaa <natan.picanco24@gmail.com>
2025-03-20 10:26:21 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let musicas =[
-    {titulo: 'interestelar', artista: 'natan Silva', src:'musicas/calm-space-music-312291.mp3', img:'imagens/interestelar.jpg'}
+    {titulo: 'interestelar', artista: 'natan Silva', src:'musicas/calm-space-music-312291.mp3', img:'imagens/interestelar.jpg'},
+    {titulo: 'top musica', artista: 'desconhecido', src:'musicas/lake-view-lo-fi-beat-03-312345.mp3', img:'imagens/logan-weaver-lgnwvr-kcKIUXkA5EM-unsplash.jpg'},
+    {titulo: 'funk', artista: 'desconhecido', src:'musicas/funk-244706.mp3', img:'imagens/dwayne-joe-ORL32yX-f9I-unsplash.jpg'}
 ];
+    
 
 
 let musica = document.querySelector('audio');
+let indexMusica =0;
 
 let duracaoMusica = document.querySelector('.fim')
 let imagem = document.querySelector('img');
 let nomeMusica = document.querySelector('.descricao h2');
-let nomwArtista = document.querySelector('.descricao i')
+let nomeArtista = document.querySelector('.descricao i')
 
-duracaoMusica.textContent = segundosParaMinutos(Math. floor(musica.duration));
+renderizarMusica(indexMusica);
+
 
 //EVENTOS
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
@@ -19,7 +24,35 @@ document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
 musica.addEventListener('timeupdate', atualizarBarra);
 
+document.querySelector('.anterior').addEventListener('click', () => {
+    indexMusica--;
+    if(indexMusica < 0){
+        indexMusica = 2
+    }
+    renderizarMusica(indexMusica);
+});
+
+document.querySelector('.proxima').addEventListener('click', () => {
+    indexMusica++;
+    if(indexMusica > 2){
+        indexMusica =0
+    }
+    renderizarMusica(indexMusica);
+});
+
 //FUNÇÕES
+
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src)
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent=musicas[index].titulo;
+        nomeArtista.textContent=musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math. floor(musica.duration));
+    }); 
+}
+
+
 function tocarMusica(){
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
@@ -48,9 +81,3 @@ function segundosParaMinutos(segundos){
 
     return campoMinutos+':'+campoSegundos;
 }
-
-
-
-
-
-
